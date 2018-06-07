335202ea1f81d41c20fb8c8c12e9685592711088
Stuart Lacy <stuart.lacy@york.ac.uk>
2018-06-07 15:16:15 +0000

    version 1.0.0

--- inst/doc/user_guide.R
+++ inst/doc/user_guide.R
@@ -6,105 +6,24 @@ data(prevsim)
 ## ------------------------------------------------------------------------
 summary(prevsim)
 
-## ----basicsurvival, fig.height=4, fig.width=7----------------------------
+## ----basicsurvival, fig.height=4, fig.width=7, echo=F--------------------
 survf <- survfit(Surv(time, status) ~ sex, data=prevsim)
-plot(survf, lwd=1, col = c("blue","red"), main="Survival stratified by sex", xlab="Days", 
-     ylab="Probability")
-legend(3000, 1, c("Males", "Females"), lty = 1, col=c("blue","red"))
+survf_df <- data.frame(t=survf$time, s=survf$surv, sex=rep(c('M', 'F'), survf$strata))
+ggplot2::ggplot(survf_df, ggplot2::aes(x=t, y=s, colour=sex)) +
+    ggplot2::geom_line() +
+    ggplot2::theme_bw() +
+    ggplot2::labs(x='Time (days)', y='Survival probability') +
+    ggplot2::ylim(0, 1)
 
 ## ------------------------------------------------------------------------
-inc <- incidence(prevsim$entrydate, population_size=3.2e6, start='2005-09-01', num_reg_years=5)
-
-## ------------------------------------------------------------------------
-inc
-names(inc)
-
-## ------------------------------------------------------------------------
-summary(inc)
-
-## ----incidencerate, error = TRUE-----------------------------------------
-inc$mean
-
-## ---- fig.width = 7, fig.height = 4--------------------------------------
-plot(inc)
-
-## ----incidenceage, fig.width = 7, fig.height = 4, error = TRUE-----------
-prevsim_r <- prevsim[prevsim$entrydate >= "2004-01-30", ]
-
-incidence_age_distribution(prevsim_r$age)
-
-## ----survivaldiag, fig.width = 7, fig.height = 4-------------------------
-km <- survfit(Surv(time, status) ~ 1, data=prevsim_r)
-plot(km, lwd=2, col="blue", main="Overall Survival", xlab="Days", 
-     ylab="Survival probability")
-
-## ----survivaldiag2, fig.width = 7, fig.height = 4------------------------
-ages = c(55, 65, 75, 85, 100)
-km2 <- survfit(Surv(time, status) ~ cut(age, breaks=ages), data=prevsim_r)
-plot(km2, lwd=2, col=1:length(ages), main="Survival stratified by age", xlab="Days", 
-     ylab="Survival probability")
-legend("topright", legend=substring(names(km2$strata), 25, 32), lty = 1, 
-       col=1:length(ages))
-
-## ----survivaldiag4, fig.width = 7, fig.height = 4, results='hide'--------
-plot(km, lwd=2, col="blue", mark.time=F, conf.int=T, xlab="Days", 
-     ylab="Survival probability")
-num_reg_years <- 9
-registry_years <- determine_yearly_endpoints(date='2004-01-30', 
-                                             num_years=num_reg_years)
-sapply(seq(num_reg_years),
-       function(i) lines(survfit(Surv(time, status) ~ 1, 
-                                 data=prevsim_r[prevsim_r$entrydate >= 
-                                                          registry_years[i] & 
-                                                          prevsim_r$entrydate < 
-                                                          registry_years[i + 1], ]), 
-                         mark.time = F, conf.int = F))
-
-## ----survivaldiag3, fig.width = 7, fig.height = 4------------------------
-cx <- coxph(Surv(time, status) ~ age, data=prevsim_r)
-cxp <- survfit(cx, 
-               newdata=data.frame(age=sapply(seq(length(ages) - 1), 
-                                             function(i) mean(c(ages[i], ages[i + 1]))))) 
-plot(cox.zph(cx))
-lines(cxp, lwd=2, col=1:length(ages), lty=2, mark.time=F)
-
-
-## ------------------------------------------------------------------------
-cox.zph(cx)
-
-## ----ageform2, fig.width = 7, fig.height = 4, error = TRUE---------------
-functional_form_age(Surv(time, status) ~ age, prevsim_r, df=4, plot_fit=T)
-
-## ----weibull, error = TRUE-----------------------------------------------
-wb <- survreg(Surv(time, status) ~ age + sex, data=prevsim_r)
-summary(wb)
-
-## ----popsurv, fig.width = 7, fig.height = 4, error = TRUE----------------
-data(UKmortality)
-
-daily_survival_males <- population_survival_rate(rate ~ age, subset(UKmortality, 
-                                                                    sex == 0))
-daily_survival_females <- population_survival_rate(rate ~ age, subset(UKmortality, 
-                                                                      sex == 1))
-
-plot(daily_survival_males, type="l", col="blue", xlab="days", ylab="survival")
-lines(daily_survival_females, col="red")
-legend("topright", legend = c("Males", "Females"),
-               bty = "n", lty = 1, col = c(4,2))
-
-## ----survival, error = TRUE----------------------------------------------
-prevalence_counted(prevsim$entrydate, 
-                   prevsim$eventdate, 
-                   prevsim$status, 
-                   index_date="2013-01-30",
-                   num_reg_years=9) 
-
-## ----prevalencetotal, error = TRUE---------------------------------------
-prevalence_total <- prevalence(Surv(time, status) ~ sex(sex) + age(age) + 
-                                   entry(entrydate) + event(eventdate),
-                               prevsim, num_years_to_estimate=c(3, 5, 10), 
-                               index_date='2013-01-30',
-                               num_reg_years=9, cure=5) 
+prevalence_total <- prevalence(index='2013-01-30', 
+                               num_years_to_estimate=c(3, 5, 10, 20), 
+                               data=prevsim, 
+                               inc_formula = entrydate ~ sex,
+                               surv_formula = Surv(time, status) ~ age + sex, 
+                               dist='weibull', 
+                               population_size = 1e6,
+                               death_column = 'eventdate')
 
 ## ------------------------------------------------------------------------
 prevalence_total
@@ -116,29 +35,212 @@ summary(prevalence_total)
 prevalence_total$estimates
 
 ## ------------------------------------------------------------------------
-prevsurv <- survfit(prevalence_total, newdata=list(age=60, sex=0))
-prevsurv
+head(prevalence_total$simulated)
 
 ## ------------------------------------------------------------------------
-summary(prevsurv, years=c(1, 3, 5, 10))
+llog <- flexsurv::flexsurvreg(Surv(time, status) ~ age + sex, data=prevsim, dist='llogis')
+llog
 
-## ---- fig.width=7, fig.height=4------------------------------------------
-plot(prevsurv, pct_show=0.90)
+## ---- cache=T------------------------------------------------------------
+prev_llog <- prevalence(index='2013-01-30', 
+                        num_years_to_estimate=c(3, 5, 10, 20), 
+                        data=prevsim, 
+                        inc_formula = entrydate ~ sex,
+                        surv_model=llog,
+                        population_size = 1e6,
+                        death_column = 'eventdate',
+                        N_boot = 100)
+
+## ---- cache=T------------------------------------------------------------
+prev_llog
 
 ## ------------------------------------------------------------------------
-prevalence_total$pval
+fix_cure_mod <- fixed_cure(Surv(time, status) ~ age + sex, data=prevsim, cure_time=5*365.25, 
+                           dist='weibull')
 
-## ----test, error = TRUE--------------------------------------------------
-test_prevalence_fit(prevalence_total)
+## ------------------------------------------------------------------------
+prevalence(index='2013-01-30', 
+           num_years_to_estimate=20, 
+           data=prevsim, 
+           inc_formula = entrydate ~ sex,
+           surv_model=fix_cure_mod,        # Pass in the cure model that was built above
+           population_size = 1e6,
+           death_column = 'eventdate',
+           N_boot = 30)
 
-## ---- fig.height = 4, fig.width = 7--------------------------------------
-hist(prevsim$age[prevsim$entrydate >= min(registry_years) & 
-                     prevsim$entrydate < max(registry_years)], 
-     col=rgb(1,0,0, alpha=0.5), xlim=c(0,100), ylim=c(0,0.045), 
-     main = "", xlab = "age", prob = TRUE)
-hist(unlist(prevalence_total$simulated$posterior_age),
-     col=rgb(0,1,0, alpha=0.5), prob = TRUE, add=T)
-legend("topleft", legend = c("Incident", "Prevalent"), bty = "n", lty = 1, 
-       col = c(rgb(1,0,0, alpha=0.5),
-               rgb(0,1,0, alpha=0.5)))
+## ------------------------------------------------------------------------
+prevalence_total
+
+## ------------------------------------------------------------------------
+mixture_cure <- flexsurvcure::flexsurvcure(Surv(time, status) ~ age + sex, data=prevsim, dist='weibull', link='logistic', mixture=TRUE)
+
+## ------------------------------------------------------------------------
+prevalence(index='2013-01-30', 
+           num_years_to_estimate=20, 
+           data=prevsim, 
+           inc_formula = entrydate ~ sex,
+           surv_model=mixture_cure,
+           population_size = 1e6,
+           death_column = 'eventdate',
+           N_boot = 30)
+
+## ---- eval=F-------------------------------------------------------------
+#  build_my_survival_object <- function(formula, input_data) {
+#      # Build a survival model using the specified formula and input data
+#      model <- ...
+#      object <- list(model=model,
+#                     call=match.call())  # the function call must be included as an item 'call'
+#      class(object) <- "myobj"
+#      object
+#  }
+
+## ---- eval=F-------------------------------------------------------------
+#  data <- data.frame(...)
+#  myobj <- build_my_survival_object(Surv(time, status) ~ sex, data)
+#  prevalence(...
+#             surv_model=myobj, # This will work
+#             ...)
+
+## ---- eval=F-------------------------------------------------------------
+#  some_data <- data.frame(...)
+#  myobj <- build_my_survival_object(Surv(time, status) ~ sex, some_data)
+#  prevalence(...
+#             surv_model=myobj, # This WON'T work, since the data parameter was called 'some_data' instead
+#             ...)
+
+## ------------------------------------------------------------------------
+build_poisson <- function(input_data) {
+    rate <- nrow(input_data) / as.numeric(difftime(max(input_data$entrydate), min(input_data$eventdate)))
+    # Build a survival model using the specified formula and input data
+    object <- list(rate=rate,
+                   call=match.call())  # the function call must be included as an item 'call'
+    class(object) <- "pois"
+    object
+}
+
+## ------------------------------------------------------------------------
+data <- prevsim
+pois_mod <- build_poisson(input_data=data)
+
+## ------------------------------------------------------------------------
+pois_mod
+
+## ---- eval=F-------------------------------------------------------------
+#  # object: The incidence model that will have been created on the bootstrapped data
+#  # data: The data available to fit the model on, will be the registry data set provided to prevalence as this acts as the attribute prior distribution.
+#  # timeframe: A single number specifying how long to generate incident cases for.
+#  # covars: A character vector specifying the names of individual covariates that must be included in the returned data.table (or data frame)
+#  
+#  # Returns a data.table (or data frame but data.table is preferred) where each row represents an incident case with:
+#  #   - The first column being the time since the origin, i.e. index date - N year prevalence
+#  #   - Any subsequent columns holding covariates that must be provided as specified in the 'covars' argument
+#  draw_incident_population <- function(object, data, timeframe, covars, ...)
+
+## ------------------------------------------------------------------------
+draw_incident_population.pois <- function(object, data, timeframe, covars, ...) {
+    # Firstly draw inter-arrival times in the period [0, timeframe]. 
+    # The expected number is simply timeframe * rate so we'll take this amount + a margin for error.
+    expected <- 1.5 * (timeframe * object$rate)
+    # Now draw inter-arrival times
+    inter_arrival <- rexp(expected, object$rate)
+    # Determine absolute incident times
+    incident_times <- cumsum(inter_arrival)
+    # Truncate to those within the timeframe
+    incident_times <- incident_times[incident_times < timeframe]
+    num_incident <- length(incident_times)
+    
+    # Sample individual attributes into a matrix. The required attributes are given by 'covars' argument
+    attrs <- do.call('cbind', lapply(covars, function(x) sample(data[[x]], num_incident, replace=T)))
+    
+    # Now add the incident times as the first column
+    attrs <- cbind(incident_times, attrs)
+    
+    # Convert to data frame and add column names
+    attrs <- data.frame(attrs)
+    colnames(attrs) <- c('incident_time', covars)
+    
+    # Return this data frame
+    attrs
+}
+
+## ------------------------------------------------------------------------
+inc_times <- validate_incidence_model(pois_mod, prevsim)
+
+## ------------------------------------------------------------------------
+head(inc_times)
+
+## ------------------------------------------------------------------------
+prevalence(index='2013-01-30', 
+           num_years_to_estimate=c(3, 5, 10, 20), 
+           data=prevsim, 
+           inc_model = pois_mod,
+           surv_formula = Surv(time, status) ~ age + sex, 
+           dist='weibull', 
+           population_size = 1e6,
+           incident_column = 'entrydate',
+           death_column = 'eventdate')
+
+## ------------------------------------------------------------------------
+library(flexsurv)
+build_wei <- function(data) {
+    mod <- flexsurvreg(Surv(time, status) ~ age, data=data, dist='weibull')
+    obj <- list(coefs=coef(mod),
+                call=match.call())
+    class(obj) <- 'mysurv'
+    obj
+}
+
+## ------------------------------------------------------------------------
+survobj <- build_wei(data=data)
+survobj
+
+## ---- eval=F-------------------------------------------------------------
+#  # object: The survival model
+#  
+#  # Returns a character vector detailing the covariates required to fit this model. All of
+#  # these values should be columns in the data that is passed in the main 'prevalence' function.
+#  extract_covars <- function(object)
+
+## ------------------------------------------------------------------------
+extract_covars.mysurv <- function(object) {
+    "age"
+}
+
+## ---- eval=F-------------------------------------------------------------
+#  # object: The survival object
+#  # newdata: A data frame (or data.table) with the incident population stored with their
+#  #   required covariates for the model.
+#  # times: A vector of times to estimate survival probability at for individuals in
+#  #   corresponding rows of 'newdata'. This should be the same length as there are
+#  #   rows in 'newdata' since each individual has their survival probability estimated once.
+#  
+#  # Returns:
+#  #  A vector of survival probabilities of length equal to the length of 'times' and the
+#  #  number of rows in 'newdata'.
+#  predict_survival_probability <- function(object, newdata, times)
+
+## ------------------------------------------------------------------------
+predict_survival_probability.mysurv <- function(object, newdata, times) {
+    # Calculate linear predictor, this will form the shape parameter
+    shape <- exp(object$coefs[1] + newdata$age*object$coefs[3])
+    scale <- exp(object$coefs[2])
+    1 - pweibull(times, shape, scale)
+}
+
+## ------------------------------------------------------------------------
+predict_survival_probability(survobj, newdata=data.frame(age=c(50, 70)), times=c(100, 100))
+
+## ------------------------------------------------------------------------
+probs <- validate_survival_model(survobj, prevsim)
+head(probs)
+
+## ------------------------------------------------------------------------
+prevalence(index='2013-01-30', 
+           num_years_to_estimate=c(3, 5, 10, 20), 
+           data=prevsim, 
+           inc_formula = entrydate ~ 1,
+           surv_model = survobj,
+           population_size = 1e6,
+           death_column = 'eventdate',
+           N_boot = 100)
 
